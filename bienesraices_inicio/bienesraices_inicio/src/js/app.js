9851abcd28954095fc157aa535c9b668eeeac7f2
Darcher08 <148842609+Darcher08@users.noreply.github.com>
2026-01-13 00:34:07 -0600

    DarkMode
    Adding dark mode to the pages

--- bienesraices_inicio/bienesraices_inicio/src/js/app.js
+++ bienesraices_inicio/bienesraices_inicio/src/js/app.js
@@ -1,6 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
     eventListeners();
+    darkMode();
 });
+function darkMode() {
+    const botonDark = document.querySelector('.dark-mode-boton');
+    botonDark.addEventListener('click', function () {
+        document.body.classList.toggle('dark-mode');
+
+    })
+}
 
 function eventListeners() {
     const mobileMenu = document.querySelector('.mobile-menu');
